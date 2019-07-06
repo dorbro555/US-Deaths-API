703a4f1af0f77876e5c894a54e7ffc9b1c4f5e3f
Glitch (helix-form-1) <none>
2019-07-06 07:18:02 +0000

    😋🌵 Checkpoint ./server.js:1934267/159

--- server.js
+++ server.js
@@ -26,7 +26,16 @@ let db = new sqlite3.Database('./app/sql/death-db',sqlite3.OPEN_READONLY ,(err) 
   console.log('Connected to the Us Deaths Database');
 });
 
+// perform sqlite query
 let sql = `SELECT * FROM US_Deaths WHERE Quantifier="Deaths Per Year"`;
+db.all(sql,[],(err, rows) => {
+  if(err){
+    throw(err);
+  }
+  rows.forEach((row)=>{
+    
+  });
+});
 
 //close sqlite database
 db.close((err) => {
