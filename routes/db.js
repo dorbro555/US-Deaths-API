b06822d4b3a1a8d63bb264f1dd1051f277f54f78
Glitch (helix-form-1) <none>
2019-07-06 16:58:02 +0000

    🐓⛳️ Checkpoint ./server.js:1934267/767 ./routes/db.js:1934267/2098

--- routes/db.js
+++ routes/db.js
@@ -1,8 +1,42 @@
 var express = require('express')
 var router = express.Router()
 
+
+// setup sqlite and access database
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./app/sql/death-db',sqlite3.OPEN_READONLY ,(err) => {
+  if(err){
+    console.error(err);
+  }
+  console.log('Connected to the Us Deaths Database');
+});
+
 router.get('/tester', (req, res) => {
+
+  // perform sqlite query
+  let sql = `SELECT * 
+             FROM US_Deaths 
+             WHERE Quantifier=?`;
+  let quantifier = 'Deaths Per Year'
+
+  db.all(sql,[quantifier],(err, rows) => {
+    if(err){
+      throw(err);
+    }
+    rows.forEach((row)=>{
+      console.log(row)
+    });
+  });
+  
   res.send('This is a test')
 })
+
+//close sqlite database
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Closed the database connection.');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,14 +3,12 @@
 // init project
 var express = require('express');
 var app = express();
-var US = require('routes/db.js');
+var US_db = require('./routes/db.js');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-app.get("/test",  (req, res) => {
-  res.send("This is a test response")
-});
+app.get("/db",US_db);
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("*", function(request, response) {
@@ -23,34 +21,3 @@ var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-// setup sqlite and access database
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./app/sql/death-db',sqlite3.OPEN_READONLY ,(err) => {
-  if(err){
-    console.error(err);
-  }
-  console.log('Connected to the Us Deaths Database');
-});
-
-// perform sqlite query
-let sql = `SELECT * 
-           FROM US_Deaths 
-           WHERE Quantifier=?`;
-let quantifier = 'Deaths Per Year'
-
-db.all(sql,[quantifier],(err, rows) => {
-  if(err){
-    throw(err);
-  }
-  rows.forEach((row)=>{
-    console.log(row)
-  });
-});
-
-//close sqlite database
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Closed the database connection.');
-});
