626489886cbedbd83d4f910456bfccd93548b94f
Glitch (helix-form-1) <none>
2019-07-06 05:18:02 +0000

    ☎️🐏 Checkpoint ./server.js:1934267/136

--- server.js
+++ server.js
@@ -22,5 +22,12 @@ let db = new sqlite3.Database('./app/sql/death-db',sqlite3.OPEN_READONLY ,(err) 
   if(err){
     console.error(err);
   }
-  console.log('Connected to the Us Deaths Database')
+  console.log('Connected to the Us Deaths Database');
+});
+
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Closed the database connection.');
 });
