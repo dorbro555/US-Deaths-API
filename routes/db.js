407e037f70993203cafe7989dfeb85ab3f4e7f8f
Glitch (helix-form-1) <none>
2019-07-06 17:31:02 +0000

    🍞😽 Checkpoint ./routes/db.js:1934267/1436

--- routes/db.js
+++ routes/db.js
@@ -6,12 +6,40 @@ var router = express.Router()
 const sqlite3 = require('sqlite3').verbose();
 
 router.get('/', (req, res) => {
- 
   
-  res.send('This is a test')
+  let db = new sqlite3.Database('./app/sql/death-db',sqlite3.OPEN_READONLY ,(err) => {
+    if(err){
+      console.error(err);
+    }
+    console.log('Connected to the Us Deaths Database');
+  });
+  
+  // perform sqlite query
+  let sql = `SELECT * 
+             FROM US_Deaths 
+             WHERE Quantifier=?`;
+  let quantifier = 'Deaths Per Year'
+  let stringBuilder = "";
+
+  db.all(sql,[quantifier],(err, rows) => {
+    if(err){
+      throw(err);
+    }
+    rows.forEach((row)=>{
+      stringBuilder = stringBuilder + row + '\n';
+    });
+    console.log(rows)
+  });
+  
+  res.send(stringBuilder)
 
   //close sqlite database
-  
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Closed the database connection.');
+  });
 })
 
 module.exports = router;
