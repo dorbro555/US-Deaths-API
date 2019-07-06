1dd70f1f0d76784b740e9b61b0521b1a0f6091cb
Glitch (helix-form-1) <none>
2019-07-06 15:52:02 +0000

    ⛲️💵 Checkpoint ./server.js:1934267/1119 ./db.js:1934267/200

--- server.js
+++ server.js
@@ -3,18 +3,20 @@
 // init project
 var express = require('express');
 var app = express();
+//var US_db = require('./db.js');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + '/app/index.html');
-});
-
 app.get("/test",  (req, res) => {
   res.send("This is a test response")
 });
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("*", function(request, response) {
+  response.sendFile(__dirname + '/app/index.html');
+});
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
