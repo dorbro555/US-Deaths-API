1e32b5a29e26bcbee34c8f7f99f421f6e6fad434
Glitch (helix-form-1) <none>
2019-07-08 05:22:06 +0000

    🚐⚓️ Checkpoint ./app/components/Table.jsx:1934267/1950

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -15,9 +15,34 @@ class Table extends React.Component {
         <table class='table'>
           <thead>
             <tr>
-              <th></th>
+              <th><abbr>Type</abbr></th>
+              <th>2000</th>
+              <th>2001</th>
+              <th>2002</th>
+              <th>2003</th>
+              <th>2004</th>
+              <th>2005</th>
+              <th>2006</th>
+              <th>2007</th>
+              <th>2008</th>
+              <th>2009</th>
+              <th>2010</th>
+              <th>2011</th>
+              <th>2012</th>
+              <th>2013</th>
+              <th>2014</th>
+              <th>2015</th>
+              <th>2016</th>
             </tr>
           </thead>
+          <tbody>
+            <tr>
+              <th><abbr title='Firearm'>F</abbr></th>
+              <tr>2356641</tr>
+              <tr>2356641</tr>
+              <tr>2356641</tr>
+            </tr>
+          </tbody>
         </table>
       </div>
     )
