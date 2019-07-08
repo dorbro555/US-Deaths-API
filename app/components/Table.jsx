82bb7bf7071e3b2e0956b04f54f989c6679b4504
Glitch (helix-form-1) <none>
2019-07-08 17:13:25 +0000

    🏟🎪 Checkpoint ./app/components/Table.jsx:1934267/2743

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -22,11 +22,15 @@ class Table extends React.Component {
     const rows = this.state.rows.map(row => {return Object.keys(row)})
     const headers = Object.keys(this.state.rows[0]).map((key,i) => {if (key!='Quantifier') return <th key={key+i}>{key}</th>})
     const headers2 = this.state.rows.map((row,i) => {return <th key={i}>{row.Quantifier}</th>})
-    const data = this.state.rows.map((row,i) => {return <tr><th><abbr title={row.Quantifier}>{i}</abbr></th></tr>})
+    const data = this.state.rows.map((row,i) => {return <tr>
+                                                          <th key={i}><abbr title={row.Quantifier}>{i}</abbr></th>
+                                                          {Object.keys(row).map((key, j) => {
+                                                            return <td key={key+j}>{row[key]}</td>})}
+                                                        </tr>})
     return(
       <div>
         Table
-        <table class='table'>
+        <table class='table' style={{overflow:'auto'}}>
           <thead>
             <tr>
               <th><abbr>Type</abbr></th>
@@ -46,6 +50,7 @@ class Table extends React.Component {
               <td>2356641</td>
               <td>2356641</td>
             </tr>
+            {data}
           </tbody>
         </table>
       </div>
