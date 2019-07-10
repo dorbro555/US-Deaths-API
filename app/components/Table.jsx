56e423667a77536ac00378bf6c8ab7ff947bfe34
Glitch (helix-form-1) <none>
2019-07-10 09:44:04 +0000

    🍜🏯 Checkpoint ./app/components/Table.jsx:1934267/1170 ./app/components/abbrLists.jsx:1934267/1186

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -1,34 +1,5 @@
 const React = require('react')
-
-const abbrMap ={
-  'US Total Population':'US Pop',
-  'Deaths Per Year':'DPY',
-  'Estimated Firearm Owners in USA (based on 30% polls)':'Est Firearm Owners',
-  'Firearms Includes Assault + Suicide Data':'Gun+As+Sui',
-  'Motor Vehicle Deaths':'MVD',
-  'Poison Deaths (All Intents)':'Poi',
-  'Poison Deaths (Accidental)':'Poi(A)',
-  'Falling Deaths ':'Fall',
-  'Alcohol Induced Deaths':'Alch',
-  'Drug Induced Deaths':'Drugs',
-  'Assault (resulting in death)  Includes Firearm data':'Assault',
-  'Intentional Self Harm (Suicide) Includes Firearm data':'Self Harm',
-  'Malignant Neoplasms (MN) - Cancers':'MN-Cancers',
-  'MN of Trachea, Bronchus, lung':'MN-Tra,Bro,Lung',
-  'MN of lymphoid, hematopoietic':'MN-Lymph',
-  'MN of Colon':'MN-Colon',
-  'MN of Pancreas':'MN-Panc',
-  'MN of Breast':'MN-Breast',
-  'MN of Other':'MN-Other',
-  'Major Cardiovascular Diseases (MC)':'MC',
-  'MC of Heart':'MC-Heart',
-  'Pneumonia':'Pneum',
-  'Chronic Lower Resp':'Chro-Resp',
-  'Chronic Liver Disease':'Chro-Liv',
-  'Renial Failure (Kidney)':'RF(Kidn)',
-  'Diabetes':'Diabetes',
-  '\'Alzheimer\'\'s\'':'Alzheimer\'s',
-}
+const abbrMap = require('./abbrLists')
 
 class Table extends React.Component {
   constructor(props){
