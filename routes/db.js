7e75e062fbd0abe56935e0e9c216a1ab66a3cd1b
Glitch (helix-form-1) <none>
2019-07-10 09:55:04 +0000

    💅🐛 Checkpoint ./routes/db.js:1934267/670

--- routes/db.js
+++ routes/db.js
@@ -36,4 +36,35 @@ router.get('/', (req, res) => {
   });
 })
 
+router.get('/:dataset', (req, res) => {
+  
+  let db = new sqlite3.Database('./app/sql/death-db',sqlite3.OPEN_READONLY ,(err) => {
+    if(err){
+      console.error(err);
+    }
+    console.log('Connected to the Us Deaths Database');
+  });
+  
+  // perform sqlite query
+  let sql = `SELECT * 
+             FROM req.params.dataset`;
+  let quantifier = 'Deaths Per Year'
+
+  db.all(sql,[],(err, rows) => {
+    if(err){
+      throw(err);
+    }
+    res.send(rows);
+  });
+  
+
+  //close sqlite database
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Closed the database connection.');
+  });
+})
+
 module.exports = router;
