14bd570c99d37a0ab4c5378a929144662a7adf77
Glitch (helix-form-1) <none>
2019-07-12 15:12:10 +0000

    📍🚁 Checkpoint ./app/components/Form.jsx:1934267/166

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -26,13 +26,13 @@ class DataForm extends React.Component {
           Pick your favorite flavor:
           <select value={this.state.value} onChange={this.handleChange}>
             <option value="/db">High Level Overview</option>
-            <option value="/db">Lime</option>
-            <option value="/db">Coconut</option>
-            <option value="/db">Mango</option>
-            <option value="/db">Mango</option>
-            <option value="/db">Mango</option>
-            <option value="/db">Mango</option>
-            <option value="/db">Mango</option>
+            <option value="/db/Breakdown_Overview">Lime</option>
+            <option value="/db/Breakdown_Race">Coconut</option>
+            <option value="/db/Breakdown_Age">Mango</option>
+            <option value="/db/State_and_USPop_Overview">Mango</option>
+            <option value="/db/State_and_USPop_Breakdown">Mango</option>
+            <option value="/db/State_and_USPop_Race">Mango</option>
+            <option value="/db/Firearm_Breakdown">Mango</option>
           </select>
         </label>
         <input type="submit" value="Submit" />
